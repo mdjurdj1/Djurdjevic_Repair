41fd9bdbaa7760d31e01ddaaf17e6d9b28817119
Mdjurdj1 <mdjurdj1@gmail.com>
2017-05-28 11:22:22 -0400

    Fixed logo resizing

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -2,14 +2,20 @@ import React, {Component} from 'react'
 import { Segment, Divider, Image } from 'semantic-ui-react'
 import './banner.css'
 
-export default class banner extends Component {
+export default class Banner extends Component {
   render() {
     return (
-      <div>
+      <div id="banner">
         <div className="ui grid">
-          <div className="six wide column" id="company_logo">
-              <Image src='../Logo.png' />
-            </div>
+
+            <Image src='../Logo.png' size="large" id="company_logo"/>
+
+          <div className="three wide column" id="contact info">
+              <h1>Contact Us!</h1>
+              <p id="phone_number">(718)-662-6765</p>
+              <p>57 Hemlock St., Valatie, NY 12184</p>
+          </div>
+
         </div>
       </div>
     )
