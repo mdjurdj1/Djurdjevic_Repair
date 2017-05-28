26434c39824d765776850325a27fbb1cb58e2771
Mdjurdj1 <mdjurdj1@gmail.com>
2017-05-28 10:36:16 -0400

    Created and added logo

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,18 +1,14 @@
 import React, {Component} from 'react'
 import { Segment, Divider, Image } from 'semantic-ui-react'
-// import logo from './repair_logo.png'
+import './banner.css'
 
 export default class banner extends Component {
   render() {
     return (
       <div>
         <div className="ui grid">
-          <div className="four wide column">
-          <Segment.Group horizontal>
-            <Segment><Image src='../repair_logo.png' size='small' /></Segment>
-            <Segment>Djurdjevic</Segment>
-            <Segment>Right</Segment>
-          </Segment.Group>
+          <div className="six wide column" id="company_logo">
+              <Image src='../Logo.png' />
             </div>
         </div>
       </div>
