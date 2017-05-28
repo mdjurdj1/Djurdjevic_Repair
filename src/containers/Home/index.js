7ee69afc847ac6e36d6e3d6f9e00cb5a6ec64a0c
Mdjurdj1 <mdjurdj1@gmail.com>
2017-05-28 14:01:27 -0400

    Added styling to contact div

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -10,7 +10,20 @@ class Home extends Component {
        <div className="ui divider"></div>
        <p className="blue">Need Computer Help? Come call or visit us and we will fix your problem!</p>
        <p />
-       <p>Here at Djurdjevic repair, we aim to provide fast service.</p>
+       <p>Here at Djurdjevic repair, we aim to provide lightning-fast, professional computer repairs within a single business day.
+       Can't bring your computer here? We can come over to your house instead!
+        </p>
+       <p>Here are some of the problems we can help you with! </p>
+       <ul>
+        <li>Virus/Spyware Removal</li>
+        <li>PC Tune-Ups</li>
+        <li>Data Backup and Restore</li>
+        <li>Data Transfer</li>
+        <li>Building Custom Computers</li>
+        <li>Upgrade Hard-Drive, Memory, Graphics Card, & More!</li>
+        <li>Full Installation of New Computers</li>
+        <li>Installing Software</li>
+       </ul>
      </Container>
     );
   }
