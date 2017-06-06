1c7feba01f8e79e3578cd3bed9a54dad464ad106
Mdjurdj1 <mdjurdj1@gmail.com>
2017-06-05 20:24:06 -0400

    Typo fixed

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -6,7 +6,7 @@ export default class Contact extends Component {
   render() {
     return (
       <Container text id="contact_div">
-         <Header as='h1' id="contact">Contact us and let's see if we can help!</Header>
+         <Header as='h1' id="contact">Contact us and we'll see if we can help!</Header>
           <div className="ui divider"></div>
           <p className="contact_p">Phone: 518-610-8190</p>
           <p className="contact_p">Cell: 718-662-6765</p>
